2d9bb070a89e5e1ff6ddcf6a6aeb20bd364dd6ec
Reichen Brewer <rjb4434@mavs.uta.edu>
2025-11-18 15:12:00 -0600

    Added Search Function
    Can now use Search via console Commands

--- OOP_Project/src/idkHowPackagesWorkTBH/DonationBank.java
+++ OOP_Project/src/idkHowPackagesWorkTBH/DonationBank.java
@@ -11,25 +11,115 @@ public class DonationBank {
 	final static String fileName = "data.txt";
 	
 	public static void main(String[] args) throws IOException{
-		
-		System.out.print("Hello, and welcome to the Donation Bank's Database, how can we help you today?"
-						+ "1. Donate an Item"
-						+ "2. Search for an Item"
-						+ "3. Purchase an Item"
-						+ "4. See Admin Commands"
-				);
+		int choice = 0;
 		readData(fileName);
-		for(int i = 0; i < items.size(); i++) {
-			items.get(i).displayItem();
+		Scanner sc = new Scanner(System.in);
+		while(choice != 5) {
+			System.out.print("Hello, and welcome to the Donation Bank's Database, how can we help you today?"
+							+ "\n1. Donate an Item"
+							+ "\n2. Search for an Item"
+							+ "\n3. Purchase an Item"
+							+ "\n4. See Admin Commands"
+							+ "\n5. Exit\n"
+					);
+			choice = sc.nextInt();
+			sc.nextLine();
+			
+			
+			switch (choice) {
+			// Donating an Item
+			case 1:
+				System.out.print("Name of the Item you are donating: ");
+				String name = sc.nextLine();
+				boolean alreadyExists = false;
+				Item donation;
+				for(int i = 0; i < items.size(); i++) {
+					if(items.get(i).getName().equalsIgnoreCase(name)) {
+						alreadyExists = true;
+						donation = items.get(i);
+						System.out.print("Quantity of " + name + " you are donating: ");
+						int qty = sc.nextInt();
+						sc.nextLine();
+						donation.setQuantity(donation.getQuantity() + qty);
+					}
+				}
+				if(!alreadyExists) {
+					System.out.print("Quantity of " + name + " you are donating: ");
+					int qty = sc.nextInt();
+					sc.nextLine();
+					System.out.print("Brand of " + name + ": ");
+					String brand = sc.nextLine();
+					System.out.print("Type of Product " + name + " is (Food, Clothing, etc.): ");
+					String section = sc.nextLine();
+					System.out.print("Section to place " + name + " in (Breakfast, Snacks, etc): ");
+					String type = sc.nextLine();
+					System.out.print("Description of " + name + ": ");
+					String desc = sc.nextLine();
+					donation = new Item(qty, brand, name, section, type, desc);
+					items.add(donation);
+				}
+				break;
+			// Searching for an Item
+			case 2:
+				String lookup = "food";
+				System.out.print("Enter Search Term: ");
+				lookup = sc.nextLine();
+				System.out.println("\nSearch Results for Items Relating to " + lookup + ": ");
+				ArrayList<Item> search = getItemsByName(lookup);
+				search.addAll(getItemsByType(lookup));
+				search.addAll(getItemsByKeyword(lookup));
+				if(search.size() == 0) {
+					System.out.println("\nNo Results\n");
+				}
+				printAllItems(search);
+				break;
+			// Purchasing an item
+			case 3:
+				System.out.print("Name of the Item you are purchasing: ");
+				String input = sc.nextLine();
+				boolean exists = false;
+				for(int i = 0; i < items.size(); i++) {
+					if(items.get(i).getName().equalsIgnoreCase(input)) {
+						exists = true;
+						if(items.get(i).purchase()) {
+							System.out.println("Purchased " + items.get(i).getName());			
+						}else {
+							System.out.println(items.get(i).getName() + " is out of stock");		
+						}
+					}
+				}
+				if(!exists) {
+					System.out.println(input + " does not exist in our system, please try searchin for an item before purchasing.");	
+				}
+				break;
+			// Ask for a password, then give Admin commands
+			case 4:
+				//Implement Password!!!
+				adminCommands();
+				break;
+			// Exit Program
+			case 5:
+				break;
+			// In case of non valid input
+			default:
+				System.out.println("Unknown input, please input a valid number.");
+				break;
+			}
+			System.out.println("");
+			
 		}
+		sc.close();
 		writeData(fileName);
+		System.out.println("Program Exitted Safely");
 	}
 	
 	// Function that allows you to access commands general customers should not have access to.
 	public static void adminCommands() {
 		System.out.print("Thank you for Accessing the Admin Terminal, please choose the action you would like to take:"
-				+ "1. Remove an Item"
-				+ "2. Edit Product Info"
+				+ "\n1. Remove an Item"
+				+ "\n2. Edit Product Info"
+				+ "\n3. Display Full Inventory"
+				+ "\n4. Exit Admin Terminal"
 		);
 	}
 	
@@ -109,6 +199,14 @@ public class DonationBank {
 			}
 		}
 		return list;
+	}
+	
+	
+	// Function to output all data from an array of Items
+	public static void printAllItems(ArrayList<Item> items) {
+		for (Item inventory : items) {
+			inventory.displayItem();
+		}
 	}
 
 }
